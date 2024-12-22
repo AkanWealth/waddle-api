8828e0a2d8eefdb11f3728ac319f53693b1baa82
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-12-22 16:28:13 +0100

    update: user signup updated

--- src/middleware/users.midddleware.ts
+++ src/middleware/users.midddleware.ts
@@ -9,8 +9,8 @@ import { Request, Response, NextFunction } from 'express';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-// middleware for activities role based authorization
-export class ActvitiesMiddleware implements NestMiddleware {
+// middleware for user role based authorization
+export class UsersMiddleware implements NestMiddleware {
   constructor(
     private jwt: JwtService,
     private prisma: PrismaService,
@@ -29,39 +29,15 @@ export class ActvitiesMiddleware implements NestMiddleware {
     });
     delete user.password;
 
+    // Check for specific request method and route path
     if (user.role === 'Customer') {
-      if (req.method !== 'GET') {
+      if (req.method === 'GET' && req.path === '/api/v1/users/all') {
         throw new ForbiddenException('You are not authorized for this action');
       }
-    }
-
-    req.user = user;
-    next();
-  }
-}
-
-// middleware for reservation role based authoriazation
-export class ReserviationRoleAuth implements NestMiddleware {
-  constructor(
-    private jwt: JwtService,
-    private prisma: PrismaService,
-  ) {}
-
-  async use(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
-    const bearerToken = authHeader?.split(' ')[1];
-    if (!bearerToken) {
-      throw new BadRequestException('Token must be provided');
-    }
-
-    const decoded = await this.jwt.verify(bearerToken);
-    const user = await this.prisma.user.findUnique({
-      where: { id: decoded.sub },
-    });
-    delete user.password;
-
-    if (user.role === 'Customer') {
-      if (req.method !== 'GET') {
+      if (
+        req.method === 'DELETE' &&
+        req.path === `/api/v1/users/${req.params.id}`
+      ) {
         throw new ForbiddenException('You are not authorized for this action');
       }
     }
