ed54b9c8690c55ed4521be899f1634f85e945675
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-12-22 17:13:10 +0100

    update: google signup updated

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -23,10 +23,10 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 
   async validate(
     accessToken: string,
+    refreshToken: string,
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
-    console.log(accessToken, profile);
     const user = await this.authService.validateGoogleUser({
       email: profile?.emails[0].value,
       name: profile?.displayName,
