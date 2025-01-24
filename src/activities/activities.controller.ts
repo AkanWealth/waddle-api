62d7f9f04f9d5b7af3a3d9d96d8904c0cc6ebb11
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-01-24 17:31:27 +0100

    update: activity search and filter updated.

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -6,47 +6,148 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
 } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { CreateActivitiesDto, UpdateActivitiesDto } from './dto';
+import { GetUser } from '../auth/decorator/get-user.decorator';
 import {
+  ApiAcceptedResponse,
   ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
   ApiOkResponse,
   ApiParam,
+  ApiQuery,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+import { Vendor } from '@prisma/client';
+import { JwtGuard } from '../auth/guard/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiBearerAuth()
 @ApiUnauthorizedResponse({
   description: 'The user is not unathorized to perform this action',
 })
-@ApiOkResponse({ description: 'Successfull' })
+@ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
+@UseGuards(JwtGuard)
 @Controller('activities')
 export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
+  @ApiCreatedResponse({ description: 'Created Successfull' })
   @Post()
-  create(@Body() dto: CreateActivitiesDto) {
-    return this.activitiesService.create(dto);
+  @UseInterceptors(FileInterceptor('images'))
+  create(
+    @GetUser() vendor: Vendor,
+    @Body() dto: CreateActivitiesDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    if (file) {
+      try {
+        new ParseFilePipe({
+          validators: [
+            new MaxFileSizeValidator({ maxSize: 10000000 }),
+            new FileTypeValidator({ fileType: 'image/*' }),
+          ],
+        }).transform(file);
+      } catch (error) {
+        throw error;
+      }
+      return this.activitiesService.create(
+        vendor.id,
+        dto,
+        file.originalname,
+        file.buffer,
+      );
+    } else {
+      return this.activitiesService.create(
+        vendor.id,
+        dto,
+        file?.originalname,
+        file?.buffer,
+      );
+    }
   }
 
+  @ApiOkResponse({ description: 'Successfull' })
   @Get()
   findAll() {
     return this.activitiesService.findAll();
   }
 
+  @ApiOkResponse({ description: 'Successfully searched' })
+  @ApiQuery({ name: 'name', required: false, type: String })
+  @ApiQuery({ name: 'age', required: false, type: String })
+  @ApiQuery({ name: 'price', required: false, type: String })
+  @Get('search')
+  search(
+    @Query('name') name: string,
+    @Query('age') age: string,
+    @Query('price') price: string,
+  ) {
+    return this.activitiesService.search(name, age, price);
+  }
+
+  @ApiOkResponse({ description: 'Successfully filtered' })
+  @ApiQuery({ name: 'age', required: false, type: String })
+  @ApiQuery({ name: 'category', required: false, type: String })
+  @ApiQuery({ name: 'address', required: false, type: String })
+  @Get('filter')
+  filterByCriteria(
+    @Query('age') age: string,
+    @Query('category') category: string,
+    @Query('address') address: string,
+  ) {
+    return this.activitiesService.filter(age, category, address);
+  }
+
+  @ApiOkResponse({ description: 'Successfull' })
   @ApiParam({ name: 'id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.activitiesService.findOne(id);
   }
 
+  @ApiAcceptedResponse({ description: 'Data accepted' })
   @ApiParam({ name: 'id' })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateActivitiesDto) {
-    return this.activitiesService.update(id, dto);
+  @UseInterceptors(FileInterceptor('images'))
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateActivitiesDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    if (file) {
+      try {
+        new ParseFilePipe({
+          validators: [
+            new MaxFileSizeValidator({ maxSize: 10000000 }),
+            new FileTypeValidator({ fileType: 'image/*' }),
+          ],
+        }).transform(file);
+      } catch (error) {
+        throw error;
+      }
+      return this.activitiesService.update(
+        id,
+        dto,
+        file.originalname,
+        file.buffer,
+      );
+    } else {
+      return this.activitiesService.update(id, dto);
+    }
   }
 
+  @ApiNoContentResponse({ description: 'Deleted successfully' })
   @ApiParam({ name: 'id' })
   @Delete(':id')
   remove(@Param('id') id: string) {
