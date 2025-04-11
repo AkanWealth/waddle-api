615d508d23389aa61cf5adf4aede71819f86d441
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-04-11 21:04:03 +0100

    update: event and authentication updated.

--- src/auth/dto/verify.dto.ts
+++ src/auth/dto/verify.dto.ts
@@ -1,12 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class RefreshTokenDto {
+export class VerifyDto {
   @ApiProperty({
     description: 'The token',
     required: true,
-    example:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjbTV2OWV4dXgwMDAwMmhrd2Zham4zeHoxIiwiZW1haWwiOiJqZEBnbWFpbC5jb20iLCJpYXQiOjE3MzY3ODU4MDQsImV4cCI6MTczNjc4OTQwNH0.fv5CKYZ1VsyqGdSv3jWPXH8tcOHgjQ0ZesEjMGBH6-0',
+    example: '8rs4syl53gd',
   })
   @IsString({ message: 'Invalid token format' })
   @IsNotEmpty({ message: 'Token can not be empty' })
--- src/event/dto/update-event.dto.ts
+++ src/event/dto/update-event.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateEventDto } from './create-event.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto extends PartialType(CreateEventDto) {
+  @ApiPropertyOptional({
+    description: 'The published state of the event',
+    example: true,
+  })
+  @IsBoolean({ message: 'The published value must be a boolean' })
+  @IsOptional()
+  isPublished: true;
+}
