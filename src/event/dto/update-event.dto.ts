de9302dd33bbad39b55cffec8ba01fe95945f203
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-04-12 16:31:38 +0100

    update: model and dto updated for testing stage

--- src/event/dto/update-event.dto.ts
+++ src/event/dto/update-event.dto.ts
@@ -2,12 +2,4 @@ import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateEventDto } from './create-event.dto';
 import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {
-  @ApiPropertyOptional({
-    description: 'The published state of the event',
-    example: true,
-  })
-  @IsBoolean({ message: 'The published value must be a boolean' })
-  @IsOptional()
-  isPublished: true;
-}
+export class UpdateEventDto extends PartialType(CreateEventDto) {}
