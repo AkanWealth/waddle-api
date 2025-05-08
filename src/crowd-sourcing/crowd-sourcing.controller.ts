2dac5800d4e9adadc39c1424fb22ca834c647b28
PhilipOyelegbin <info@philipoyelegbin.com.ng>
2025-05-08 14:01:05 +0100

    feat: crowd sourcing created

---
+++ src/crowd-sourcing/crowd-sourcing.controller.ts
@@ -0,0 +1,158 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UploadedFiles,
+  UseInterceptors,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { CrowdSourcingService } from './crowd-sourcing.service';
+import { CreateCrowdSourcingDto, UpdateCrowdSourcingDto } from './dto';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import {
+  ApiAcceptedResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/auth/decorator/role-decorator';
+import { AdminRole } from 'src/auth/enum';
+
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@ApiInternalServerErrorResponse({ description: 'Internal server error' })
+@UseGuards(AuthGuard('jwt'))
+@Controller('crowd-sourcing')
+export class CrowdSourcingController {
+  constructor(private readonly crowdSourcingService: CrowdSourcingService) {}
+
+  @ApiOperation({
+    summary: 'create a new crowd sourcing event',
+    description: 'Create a new crowd sourcing event by parent',
+  })
+  @ApiCreatedResponse({ description: 'Created' })
+  @Post()
+  @UseInterceptors(FilesInterceptor('images'))
+  async createSourcedEvent(
+    @GetUser() user: User,
+    @Body() dto: CreateCrowdSourcingDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    const fileBuffers = files?.map((file) => file.buffer) || [];
+    const fileNames = files?.map((file) => file.originalname) || [];
+
+    return this.crowdSourcingService.createSourcedEvent(
+      user.id,
+      dto,
+      fileNames,
+      fileBuffers,
+    );
+  }
+
+  @ApiOperation({
+    summary: 'verify a new crowd sourcing event',
+    description: 'Verify a new crowd sourcing event by admin',
+  })
+  @ApiOkResponse({ description: 'Ok' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @Post('verify')
+  @Roles(AdminRole.Admin || AdminRole.Editor)
+  async verifyCrowdSourcedEvent(
+    @GetUser() user: User,
+    @Param('id') id: string,
+  ) {
+    if (user) return this.crowdSourcingService.verifyCrowdSourcedEvent(id);
+  }
+
+  @ApiOperation({
+    summary: 'view all verified crowdsourced event',
+    description: 'View all verified crowdsourced event',
+  })
+  @ApiOkResponse({ description: 'Ok' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @Get()
+  findAllSourcedEvent() {
+    return this.crowdSourcingService.findAllSourcedEvent();
+  }
+
+  @ApiOperation({
+    summary: 'view a crowdsourced event by ID',
+    description: 'View a crowdsourced event by ID',
+  })
+  @ApiOkResponse({ description: 'Ok' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @Get(':id')
+  findOneSourcedEvent(@Param('id') id: string) {
+    return this.crowdSourcingService.findOneSourcedEvent(id);
+  }
+
+  @ApiOperation({
+    summary: 'update a crowdsourced event',
+    description: 'Update a crowdsourced event by ID',
+  })
+  @ApiAcceptedResponse({ description: 'Accepted' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @HttpCode(HttpStatus.ACCEPTED)
+  @Patch(':id')
+  @UseInterceptors(FilesInterceptor('images'))
+  async updateSourcedEvent(
+    @GetUser() user: User,
+    @Param('id') id: string,
+    @Body() dto: UpdateCrowdSourcingDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    const fileBuffers = files?.map((file) => file.buffer) || [];
+    const fileNames = files?.map((file) => file.originalname) || [];
+
+    return this.crowdSourcingService.updateSourcedEvent(
+      user.id,
+      id,
+      dto,
+      fileNames,
+      fileBuffers,
+    );
+  }
+
+  @ApiOperation({
+    summary: 'delete a crowdsourced event temporarily',
+    description: 'Delete a crowdsourced event by ID temporarily',
+  })
+  @ApiNoContentResponse({ description: 'No content' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete('temp/:id')
+  @Roles(AdminRole.Admin || AdminRole.Editor)
+  removeSourcedEventTemp(
+    @GetUser() user: { id: string },
+    @Param('id') id: string,
+  ) {
+    if (user) return this.crowdSourcingService.removeSourcedEventTemp(id);
+  }
+
+  @ApiOperation({
+    summary: 'delete a crowdsourced event',
+    description: 'Delete a crowdsourced event by ID',
+  })
+  @ApiNoContentResponse({ description: 'No content' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  @Roles(AdminRole.Admin || AdminRole.Editor)
+  removeSourcedEvent(@GetUser() user: { id: string }, @Param('id') id: string) {
+    if (user) return this.crowdSourcingService.removeSourcedEvent(id);
+  }
+}
