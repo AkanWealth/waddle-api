d8203f3431eaf05719fe8501b18c38c99ff5cace
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-06-05 19:27:57 +0100

    feat: likes for comment and reply added.

--- src/like/dto/create-event-like.dto.ts
+++ src/like/dto/create-event-like.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class CreateLikeDto {
+export class CreateEventLikeDto {
   @ApiProperty({
     description: 'Event ID to like',
     example: 'hdiyir6ehef8y3883y',
