b5dd4d538e50d165b40162135e51baa96c0e3603
David Hype <iamsocialhype@gmail.com>
2025-06-18 15:50:17 +0100

    feat: added feature to get all revenue for vendors

--- prisma/migrations/20250429111246_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `email_verify` on the `vendorstaff` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "vendorstaff" DROP COLUMN "email_verify";
--- prisma/migrations/20250508115053_update/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "like" DROP CONSTRAINT "like_eventId_fkey";
-
--- AlterTable
-ALTER TABLE "event" ADD COLUMN     "likeId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "event" ADD CONSTRAINT "event_likeId_fkey" FOREIGN KEY ("likeId") REFERENCES "like"("id") ON DELETE SET NULL ON UPDATE CASCADE;
