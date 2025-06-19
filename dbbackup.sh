1aadc95a52cf127ecf4d74ce0f46a1249b90ead3
PhilipOyelegbin <philipoyelegbin@gmail.com>
2025-06-19 11:34:01 +0100

    db backup script updated

--- dbbackup.sh
+++ dbbackup.sh
@@ -2,3 +2,9 @@
 
 # This script backs takes a backup of the database schema and saves it to a file.
 pg_dump -h waddleapp-rds.c564s0oqi0ug.eu-north-1.rds.amazonaws.com -p 5432 -U root -s -F p -d waddledb > ./backup/schema.sql
+
+# backup database data to a tar file
+pg_dump -h waddleapp-rds.c564s0oqi0ug.eu-north-1.rds.amazonaws.com -p 5432 -U root -W -F t -d waddledb > ./backup/db.tar
+
+# restrore the database data from a tar file
+pg_restore -h waddleapp-rds.c564s0oqi0ug.eu-north-1.rds.amazonaws.com -p 5432 -U root -d waddledb -W ./backup/db.tar
---
+++ prisma/migrations/20250619092719_update/migration.sql
@@ -0,0 +1,377 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'ORGANISER', 'GUARDIAN');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('Pending', 'Confirmed', 'Failed', 'Cancelled', 'Refunded');
+
+-- CreateEnum
+CREATE TYPE "Tag" AS ENUM ('Event', 'Place');
+
+-- CreateEnum
+CREATE TYPE "ActivationStatus" AS ENUM ('PENDING', 'ACTIVE', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "admin" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email_verify" BOOLEAN NOT NULL DEFAULT false,
+    "verification_token" TEXT,
+    "verification_token_expiration" TEXT,
+    "reset_token" TEXT,
+    "reset_expiration" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "fcmToken" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'ADMIN',
+    "fcmIsOn" BOOLEAN NOT NULL DEFAULT false,
+    "activationStatus" "ActivationStatus" NOT NULL DEFAULT 'PENDING',
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AdminPermission" (
+    "id" TEXT NOT NULL,
+    "adminId" TEXT NOT NULL,
+    "module" TEXT NOT NULL,
+    "canCreate" BOOLEAN NOT NULL DEFAULT false,
+    "canView" BOOLEAN NOT NULL DEFAULT false,
+    "canManage" BOOLEAN NOT NULL DEFAULT false,
+    "canDelete" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "AdminPermission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organiser" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "business_logo" TEXT,
+    "paymentAccountId" TEXT,
+    "address" TEXT NOT NULL,
+    "business_name" TEXT NOT NULL,
+    "business_category" TEXT NOT NULL,
+    "registration_number" TEXT NOT NULL,
+    "email_verify" BOOLEAN NOT NULL DEFAULT true,
+    "isLocked" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "isVerified" BOOLEAN NOT NULL DEFAULT true,
+    "failedLoginAttempts" INTEGER NOT NULL DEFAULT 0,
+    "website_url" TEXT,
+    "facebook_url" TEXT,
+    "verification_token" TEXT,
+    "verification_token_expiration" TEXT,
+    "reset_token" TEXT,
+    "reset_expiration" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "fcmToken" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'ORGANISER',
+    "fcmIsOn" BOOLEAN NOT NULL DEFAULT false,
+    "isApproved" BOOLEAN NOT NULL DEFAULT false,
+    "lastLoginAt" TIMESTAMP(6),
+
+    CONSTRAINT "organiser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "profile_picture" TEXT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone_number" TEXT,
+    "address" TEXT,
+    "password" TEXT NOT NULL,
+    "guardian_type" TEXT,
+    "email_verify" BOOLEAN NOT NULL DEFAULT false,
+    "isLocked" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "failedLoginAttempts" INTEGER NOT NULL DEFAULT 0,
+    "verification_token" TEXT,
+    "verification_token_expiration" TEXT,
+    "reset_token" TEXT,
+    "reset_expiration" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "fcmToken" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'GUARDIAN',
+    "fcmIsOn" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "event" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "images" TEXT,
+    "price" DECIMAL(65,30) NOT NULL,
+    "total_ticket" INTEGER NOT NULL,
+    "ticket_booked" INTEGER NOT NULL DEFAULT 0,
+    "date" TIMESTAMP(3) NOT NULL,
+    "time" TEXT NOT NULL,
+    "age_range" TEXT NOT NULL,
+    "instruction" TEXT,
+    "category" TEXT NOT NULL,
+    "isPublished" BOOLEAN DEFAULT false,
+    "isDeleted" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "adminId" TEXT,
+    "organiserId" TEXT,
+
+    CONSTRAINT "event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "crowdsource" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "images" TEXT[],
+    "description" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "date" TIMESTAMP(3),
+    "fee" DECIMAL(65,30) NOT NULL,
+    "tag" "Tag" NOT NULL DEFAULT 'Place',
+    "time" TEXT,
+    "tips" TEXT,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "category" TEXT NOT NULL,
+    "facilities" TEXT[],
+
+    CONSTRAINT "crowdsource_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "booking" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "ticket_quantity" INTEGER NOT NULL,
+    "sessionId" TEXT,
+    "status" "BookingStatus" NOT NULL DEFAULT 'Pending',
+    "isDeleted" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "payment_intent" TEXT,
+
+    CONSTRAINT "booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "consent" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "notes" TEXT NOT NULL,
+    "consent" BOOLEAN NOT NULL,
+    "bookingId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "consent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "favorite" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "favorite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "like" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "crowdSourceId" TEXT,
+    "commentId" TEXT,
+    "reviewId" TEXT,
+
+    CONSTRAINT "like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "recommendation" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "recommendation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "comment" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "crowdSourceId" TEXT,
+    "parentId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "review" (
+    "id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "eventId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "blacklisted_token" (
+    "id" TEXT NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "blacklisted_token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admin_email_key" ON "admin"("email");
+
+-- CreateIndex
+CREATE INDEX "admin_email_idx" ON "admin"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminPermission_adminId_module_key" ON "AdminPermission"("adminId", "module");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organiser_email_key" ON "organiser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organiser_business_name_key" ON "organiser"("business_name");
+
+-- CreateIndex
+CREATE INDEX "organiser_email_idx" ON "organiser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE INDEX "user_email_idx" ON "user"("email");
+
+-- CreateIndex
+CREATE INDEX "event_category_isPublished_age_range_idx" ON "event"("category", "isPublished", "age_range");
+
+-- CreateIndex
+CREATE INDEX "crowdsource_address_isVerified_idx" ON "crowdsource"("address", "isVerified");
+
+-- CreateIndex
+CREATE INDEX "booking_userId_eventId_sessionId_status_idx" ON "booking"("userId", "eventId", "sessionId", "status");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "favorite_eventId_key" ON "favorite"("eventId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "like_userId_eventId_key" ON "like"("userId", "eventId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "like_userId_crowdSourceId_key" ON "like"("userId", "crowdSourceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "like_userId_commentId_key" ON "like"("userId", "commentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "like_userId_reviewId_key" ON "like"("userId", "reviewId");
+
+-- CreateIndex
+CREATE INDEX "comment_crowdSourceId_idx" ON "comment"("crowdSourceId");
+
+-- CreateIndex
+CREATE INDEX "review_rating_idx" ON "review"("rating");
+
+-- AddForeignKey
+ALTER TABLE "AdminPermission" ADD CONSTRAINT "AdminPermission_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "admin"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "event" ADD CONSTRAINT "event_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "admin"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "event" ADD CONSTRAINT "event_organiserId_fkey" FOREIGN KEY ("organiserId") REFERENCES "organiser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "crowdsource" ADD CONSTRAINT "crowdsource_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "booking" ADD CONSTRAINT "booking_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "booking" ADD CONSTRAINT "booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "consent" ADD CONSTRAINT "consent_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "booking"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorite" ADD CONSTRAINT "favorite_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorite" ADD CONSTRAINT "favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "like" ADD CONSTRAINT "like_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "like" ADD CONSTRAINT "like_crowdSourceId_fkey" FOREIGN KEY ("crowdSourceId") REFERENCES "crowdsource"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "like" ADD CONSTRAINT "like_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "like" ADD CONSTRAINT "like_reviewId_fkey" FOREIGN KEY ("reviewId") REFERENCES "review"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "like" ADD CONSTRAINT "like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "recommendation" ADD CONSTRAINT "recommendation_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "recommendation" ADD CONSTRAINT "recommendation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_crowdSourceId_fkey" FOREIGN KEY ("crowdSourceId") REFERENCES "crowdsource"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "review" ADD CONSTRAINT "review_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
