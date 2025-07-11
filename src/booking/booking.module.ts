95ee3495ddd0eccb4399324d0fccd3458b769eb0
David Hype <iamsocialhype@gmail.com>
2025-07-11 15:46:10 -0700

    fix: fix the notification api

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -3,8 +3,10 @@ import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { NotificationService } from '../notification/notification.service';
 import { Mailer } from '../helper';
+import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
+  imports: [NotificationModule],
   controllers: [BookingController],
   providers: [BookingService, NotificationService, Mailer],
 })
--- src/notification/dto/create-notification.dto.ts
+++ src/notification/dto/create-notification.dto.ts
@@ -1,1 +1,39 @@
-export class CreateNotificationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateNotificationDto {
+  @ApiProperty({
+    description: 'Title',
+    example: 'Event Booking Confirmed',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    description: 'Body',
+    example: 'Your booking for Kids Party has been confirmed!',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  body: string;
+
+  @ApiProperty({
+    description: 'Recipient User ID',
+    example: 'clu123456789',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  recipientId: string;
+
+  @ApiProperty({
+    description: 'Send push notification',
+    example: true,
+    required: false,
+  })
+  @IsOptional()
+  sendPush?: boolean = true;
+}
