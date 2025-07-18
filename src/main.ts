64f24dcaf8c3fca83700787ec5cc4f23aef61567
David Hype <iamsocialHype@gmail.com>
2025-07-18 18:47:17 +0100

    feat: implemented the delete Many notifications and also resolved the cors issue (#12)

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     cors: {
       origin: [
         'http://localhost:3000',
+        'http://localhost:3001',
         'http://localhost:5173',
         'http://localhost:3030',
         'https://waddle-admin.vercel.app',
